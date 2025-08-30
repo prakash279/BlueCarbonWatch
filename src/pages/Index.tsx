86561cd35414d70f8b2eb3d39b41300000a3f7f4
sanya3003 <115348466+sanya3003@users.noreply.github.com>
2025-08-30 16:28:03 +0530

    adding web interface codes

---
+++ src/pages/Index.tsx
@@ -0,0 +1,16 @@
+import Layout from '@/components/layout/Layout';
+import HeroSection from '@/components/home/HeroSection';
+import ImpactSection from '@/components/home/ImpactSection';
+import FeaturesSection from '@/components/home/FeaturesSection';
+
+const Index = () => {
+  return (
+    <Layout>
+      <HeroSection />
+      <ImpactSection />
+      <FeaturesSection />
+    </Layout>
+  );
+};
+
+export default Index;
